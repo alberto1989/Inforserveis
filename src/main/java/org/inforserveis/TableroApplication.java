9201d434d54066291050ce5462191716245d69af
heroe <heroe@192.168.1.6>
2016-05-03 22:24:04 -0500

    inyeccion dependencias

---
+++ src/main/java/org/inforserveis/TableroApplication.java
@@ -0,0 +1,12 @@
+package org.inforserveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableroApplication.class, args);
+	}
+}
